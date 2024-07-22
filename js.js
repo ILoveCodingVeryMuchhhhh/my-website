f2ca40b8d11f73da554edc7dc0fe98cc9dd6f923
L.U. <eis114514@qq.com>
2024-07-22 14:31:53 +0800

    添加了about内容

---
+++ js.js
@@ -0,0 +1,13 @@
+$(document).ready(
+    function(){
+        var btn1 = $(".btn_1");
+        var btn2 = $(".btn_2");
+        var btn3 = $(".btn_3");
+        var btn4 = $(".btn_4");
+        btn1.click(
+            function(){
+                window.location.href = "./about.html";
+            }
+        )
+    }
+)
