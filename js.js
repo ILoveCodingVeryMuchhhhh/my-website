7d04e8229bc3048facff5fe7dcbaa32ef7f27b0c
L.U. <eis114514@qq.com>
2024-07-23 12:46:32 +0800

    添加了project

--- js.js
+++ js.js
@@ -5,10 +5,6 @@ $(document).ready(
         var btn3 = $(".btn_3");
         var btn4 = $(".btn_4");
 
-        function jump(url){
-            window.location.href = url;
-        }
-
         btn1.click(function(){window.location.href="./about.html";});
         btn2.click(function(){window.location.href="./project.html";});
     }
